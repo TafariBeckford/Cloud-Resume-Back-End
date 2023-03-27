55fa231f932af011e64e5f57365ed3af42c87eb9
TafariBeckford <beckfordtafari55@gmail.com>
2023-03-27 17:12:49 -0500

    cypress

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    specPattern: "**/API/**/*.spec.js" ,
+    specPattern: "**/e2e/**/*.spec.js" ,
     supportFile: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
